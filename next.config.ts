bcda6d5473c8dbc5304568a7c0e8c150bb8c90bb
Dias Culenga Daniel <diasculengadaniel908@gmail.com>
2024-12-16 15:14:05 +0100

    Add integration with Postgres DataBase and Implementing Streaming

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /*experimental: {Cause CanaryOnlyError.
+    ppr: 'incremental',
+  },*/
 };
 
 export default nextConfig;
